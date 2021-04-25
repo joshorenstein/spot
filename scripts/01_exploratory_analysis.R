92e37602142cd59f548ee104888893a1f31e8ec9
josh <josh.orenstein@gmail.com>
2021-04-25 12:46:59 -0400

    added heat and trend index

--- scripts/01_exploratory_analysis.R
+++ scripts/01_exploratory_analysis.R
@@ -11,10 +11,15 @@ df <- read_delim("data/playlist_summary_external.txt", delim = "\t")
 #do some correlations
 cor_df <- df %>% select(streams:skippers, n_tracks:monthly_owner_stream30s)
 correlations <- cor(cor_df)
-corrplot(correlations,order="hclust") #show corrleations for fastballs
+c <- corrplot(correlations,order="hclust") #show corrleations for fastballs
 highCorr <- findCorrelation(correlations,cutoff=.7,names=TRUE) #find highly correlated predictors
 highCorr #everything is correlated!
-summary(df)
+pdf(file = "images/corrplot.pdf")
+
+corrplot(c, 
+         title = "Spotify Playlist Correlations")
+
+dev.off()
 hist(df$stream30s) #dependent variable heavily skewed
 
 df1 <- df %>% arrange(desc(stream30s)) %>% head(2000) #take a look at top 1000 rows
@@ -25,7 +30,7 @@ hist(log(df1$mau)) #fairly normal
 
 #make some stats
 df_1 <- df1 %>% 
-  mutate(trending = dau/mau,hot = wau/mau,rising=mau/(mau+mau_previous_month),
+  mutate(trending = dau/(mau-dau),hot = wau/(mau-wau),rising=mau/(mau+mau_previous_month),
          skip=skippers/users,change_rate=n_local_tracks/n_tracks)
 
 #keep independent variables and use mau as dependent variable
@@ -33,8 +38,7 @@ df_select <- df_1 %>%
   select(mau,n_tracks,n_artists,n_albums,tokens:change_rate)
 
 #Run a Mixed Model to Predict MAU
-
-m1 <- lmer(mau ~ n_tracks + n_artists + this_week + this_month 
+m1 <- lmer(monthlystre ~ n_tracks + n_artists + this_week + this_month 
            + skip + change_rate +
              + (1|genre_1/genre_2/genre_3) +
              (1|mood_1/mood_2/mood_3),data=df_select)
--- scripts/02_dashboard_stats.R
+++ scripts/02_dashboard_stats.R
@@ -8,25 +8,25 @@ library(stringr)
 dim(df)
 #read the data in
 df <- read_delim("data/playlist_summary_external.txt", delim = "\t")
-
+df <- df %>% 
+  mutate(rank = dense_rank(desc(monthly_stream30s))) %>% 
+  filter(rank<=1000) %>% 
+  mutate(tier = ifelse(rank<101, "Top 100",ifelse(rank>500,"Top 501-1000",
+                                                  "Top 101-500")))
 #make some stats
 df <- df %>% 
-  mutate(trending = dau/mau,hot = wau/mau,rising=mau/(mau+mau_previous_month),
+  mutate(trending = (dau/(mau))*100,hot = (wau/(mau))*100,rising=mau/(mau+mau_previous_month),
          skip=skippers/users,change_rate=n_local_tracks/n_tracks)
-#View(df)
+df <- df[!is.infinite(rowSums(df)),]
+df$trend1 <- scale(df$trending)
+View(df)
 df %>% 
   group_by(stream30s) %>% 
   count() %>% 
   arrange(stream30s) %>% View()
   
-d <- df %>% 
-  filter(stream30s > 100)
-names(d)
-d1 <- d %>% 
-  mutate(rank = dense_rank(desc(monthly_stream30s))) %>% 
-  filter(rank<=1000) %>% 
-  mutate(tier = ifelse(rank<101, "Top 100",ifelse(rank>500,"Top 501-1000",
-                     "Top 101-500")))
+d1 <- df
+
 
 d1$tokens <- gsub( "[^,a-zA-Z\\s]" , "" , d1$tokens , perl = TRUE )
 
