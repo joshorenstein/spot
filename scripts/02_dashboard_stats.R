fbfd98a24ba4c95a2c93ad5338cc8bef979592b6
josh <josh.orenstein@gmail.com>
2021-04-24 22:19:13 -0400

    minor updates

--- scripts/02_dashboard_stats.R
+++ scripts/02_dashboard_stats.R
@@ -9,10 +9,11 @@ dim(df)
 #read the data in
 df <- read_delim("data/playlist_summary_external.txt", delim = "\t")
 
+#make some stats
 df <- df %>% 
   mutate(trending = dau/mau,hot = wau/mau,rising=mau/(mau+mau_previous_month),
          skip=skippers/users,change_rate=n_local_tracks/n_tracks)
-View(df)
+#View(df)
 df %>% 
   group_by(stream30s) %>% 
   count() %>% 
@@ -22,15 +23,15 @@ d <- df %>%
   filter(stream30s > 100)
 names(d)
 d1 <- d %>% 
-  mutate(rank = dense_rank(desc(stream30s))) %>% 
-  filter(rank<=500) %>% 
-  mutate(tier = ifelse(rank<101, "Top 100",
-                     "Top 101-500"))
+  mutate(rank = dense_rank(desc(monthly_stream30s))) %>% 
+  filter(rank<=1000) %>% 
+  mutate(tier = ifelse(rank<101, "Top 100",ifelse(rank>500,"Top 501-1000",
+                     "Top 101-500")))
 
 d1$tokens <- gsub( "[^,a-zA-Z\\s]" , "" , d1$tokens , perl = TRUE )
 
 
 final <- d1 %>% separate(tokens, c("token_1","token_2","token_3")) 
-names(final)
+View(final)
 final %>% write_csv("spotify.csv")
 
