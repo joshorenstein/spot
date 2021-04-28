7898be319e8557ab476fdbdeabe07e557e9f6211
josh <josh.orenstein@gmail.com>
2021-04-27 22:17:26 -0400

    expand to 10000 playlists

--- scripts/01_exploratory_analysis.R
+++ scripts/01_exploratory_analysis.R
@@ -1,6 +1,5 @@
 #load some packages
 library(tidyverse)
-
 library(corrplot)
 library(caret)
 library(randomForest)
@@ -22,7 +21,7 @@ corrplot(c,
 dev.off()
 hist(df$stream30s) #dependent variable heavily skewed
 
-df1 <- df %>% arrange(desc(stream30s)) %>% head(2000) #take a look at top 1000 rows
+df1 <- df %>% arrange(desc(stream30s)) %>% head(2000) #take a look at top 2000 rows
 hist(df1$stream30s) #still skewed
 hist(log(df1$stream30s)) #still skewed
 hist(df1$mau) #try monthly actives
@@ -36,30 +35,37 @@ df_1 <- df1 %>%
 #keep independent variables and use mau as dependent variable
 df_select <- df_1 %>% 
   select(mau,n_tracks,n_artists,n_albums,tokens:change_rate)
-
+names(df_select)
 #Run a Mixed Model to Predict MAU
-m1 <- lmer(monthlystre ~ n_tracks + n_artists + this_week + this_month 
-           + skip + change_rate +
-             + (1|genre_1/genre_2/genre_3) +
-             (1|mood_1/mood_2/mood_3),data=df_select)
+m1 <- lmer(mau ~ scale(n_tracks) + scale(n_artists) + scale(trending) + 
+             scale(rising)  
+           + scale(skip) + scale(change_rate) +
+             + (1|genre_1) +
+             (1|mood_1),data=df_select)
 summary(m1) # not a whole lot there
 library(ggeffects)
 # Extract the prediction data frame - this is pointless but lets just see whats here
 pred.mm <- ggpredict(m1)  # this gives overall predictions for the model
+
+#fit the data
 df_m <- df_select %>% 
   predict(m1, newdata = .) %>% 
   bind_cols(df_select) %>% 
   rename(fit=...1)
-
+View(df_m)
 summary(df_m)
+
+# Not very interesting model fits 
 df_m %>% 
-  filter(fit > 13937) %>% 
   group_by(genre_1) %>% 
-  summarise(n=n()) %>% 
-  arrange(desc(n))
+  summarise(fit=mean(fit),mau=mean(mau)) %>% 
+  mutate(resid=fit-mau) %>% 
+  arrange(desc(resid)) %>% 
+  View()
 
 df_m %>% 
-  filter(fit > 13937) %>% 
   group_by(mood_1) %>% 
-  summarise(n=n()) %>% 
-  arrange(desc(n))
+  summarise(fit=mean(fit),mau=mean(mau)) %>% 
+  mutate(resid=fit-mau) %>% 
+  arrange(desc(resid)) %>% 
+  View()
--- scripts/02_dashboard_stats.R
+++ scripts/02_dashboard_stats.R
@@ -5,33 +5,34 @@ library(caret)
 library(randomForest)
 library(lme4)
 library(stringr)
-dim(df)
-#read the data in
-df <- read_delim("data/playlist_summary_external.txt", delim = "\t")
-df <- df %>% 
-  mutate(rank = dense_rank(desc(monthly_stream30s))) %>% 
-  filter(rank<=1000) %>% 
-  mutate(tier = ifelse(rank<101, "Top 100",ifelse(rank>500,"Top 501-1000",
-                                                  "Top 101-500")))
-#make some stats
-df <- df %>% 
-  mutate(trending = (dau/(mau))*100,hot = (wau/(mau))*100,rising=mau/(mau+mau_previous_month),
-         skip=skippers/users,change_rate=n_local_tracks/n_tracks)
-df <- df[!is.infinite(rowSums(df)),]
-df$trend1 <- scale(df$trending)
 View(df)
-df %>% 
-  group_by(stream30s) %>% 
-  count() %>% 
-  arrange(stream30s) %>% View()
-  
-d1 <- df
+#read the data in
+d <- read_delim("data/playlist_summary_external.txt", delim = "\t")
 
+#rank based on monthly streams over 30 seconds
+df <- d %>% 
+  mutate(rank = rank(-monthly_stream30s,ties.method="min")) %>% 
+  filter(rank<=10000) %>% 
+  mutate(tier = ifelse(rank<101, "Top 100",
+                ifelse(rank>500&rank<=1000,"Top 501-1000",
+                ifelse(rank<501&rank>100,"Top 101-500",
+                       "Bottom Tier"))))
 
-d1$tokens <- gsub( "[^,a-zA-Z\\s]" , "" , d1$tokens , perl = TRUE )
+View(df)
+#make some stats
+names(df)
+df <- df %>% 
+  mutate(trending = (dau/(mau))*100,
+         hot = (wau/(mau))*100,
+         rising=(mau/(mau+mau_previous_month))*100,
+         mo_chg = ifelse(mau_previous_month!=0,
+                         mau-mau_previous_month,NA),
+         skip=skippers/users,
+         change_rate=n_local_tracks/n_tracks)
 
+df$tokens <- gsub( "[^,a-zA-Z\\s]" , "" , df$tokens , perl = TRUE ) #clean the tokens column
 
-final <- d1 %>% separate(tokens, c("token_1","token_2","token_3")) 
+final <- df %>% separate(tokens, c("token_1","token_2","token_3")) #split out the first 3 tokens
 View(final)
-final %>% write_csv("spotify.csv")
+final %>% write_csv("spotify.csv") #push to csv
 
